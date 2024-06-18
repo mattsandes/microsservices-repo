41a9ff20a2a135e513c7407caba607b25ba7b6e3
Mateus Sandes <mateussandes@hotmail.com.br>
2024-06-18 11:21:20 -0300

    Atualizado o greeting controller para receber as propriedades vindas da classe GreetingConfiguration

---
+++ src/main/java/br/com/sandes/controllers/GreetingController.java
@@ -0,0 +1,34 @@
+package br.com.sandes.controllers;
+
+import br.com.sandes.configuration.GreetingConfiguration;
+import br.com.sandes.models.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "%s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfiguration configuration;
+
+    @RequestMapping("/greeting")
+        public Greeting greeting(
+                @RequestParam(value = "name",
+                        defaultValue = "") String name) {
+
+            if(name.isEmpty()){
+                name = configuration.getDefaultValue();
+            }
+
+            return new Greeting(
+                    counter.incrementAndGet(),
+                    String.format(template, configuration.getGreeting(), name));
+        }
+}
